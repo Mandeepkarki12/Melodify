b8fb5159c5d5fb7f9f8011461f54e948f819a568
karkimandeep <karki@MANDEEP>
2024-06-04 18:14:26 +0545

    Changes test

--- Form1.vb
+++ Form1.vb
@@ -2,7 +2,7 @@
     Dim SQL As New SQLControl
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If SQL.hasConnection = True Then
-            MsgBox("Connected Sucessfully")
+            MsgBox("Connected Sucessfully !!")
         End If
     End Sub
 End Class
